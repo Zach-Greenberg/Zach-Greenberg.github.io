7fee5c754b2cf44376bb11b56d6841ef7e99a246
Zinc <greenberg.zachary@utexas.edu>
2022-01-07 16:11:14 -0600

    bug fixes and smooth scrolling done

--- js/script.js
+++ js/script.js
@@ -27,4 +27,21 @@ $(document).ready(function(){
         $('body').toggleClass('dark-theme');
     });
 
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+        
+        e.preventDefault();
+
+        $('html, body').animate({
+            
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        );
+
+    });
+
 });
